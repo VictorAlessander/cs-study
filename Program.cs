7a2e3f4b17e6df9e809314daee77eacbcb642118
Debian Live user <user@localhost.localdomain>
2017-03-08 13:05:33 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Hello;
+
+namespace hwapp{
+    public class Program{
+        public static HelloWorld boas_vindas;
+
+        public static void Main(string[] args){
+            
+            boas_vindas = new Hello.HelloWorld();
+            Console.WriteLine(boas_vindas.Bemvindo());
+        }
+    }
+}
---
+++ Welcome/Helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace Hello{
+
+    public class HelloWorld{
+        
+        public string Bemvindo(){
+            return ("Hello World!");
+        }
+
+    }
+}
